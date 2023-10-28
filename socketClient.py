726ddb84f635050b9abc0bc9611788036614faac
PallaviVangari <vangaripallavi27@gmail.com>
2023-10-28 15:11:46 -0700

    first commit

---
+++ socketClient.py
@@ -0,0 +1,12 @@
+
+
+import asyncio
+from websockets.sync.client import connect
+
+def hello():
+    with connect("ws://localhost:8765") as websocket:
+        for i in range(1, 10000):
+            websocket.send("Request [" + str(i) + "] Hello world!")
+            message = websocket.recv()
+            print(f"Received: {message}")
+hello()
